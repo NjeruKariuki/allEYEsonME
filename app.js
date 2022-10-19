d3917b2cb69f3fc0632ece0d0e250b6389645a94
Daniel Kariuki <freakoutbond2@gmail.com>
2022-10-19 10:56:59 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+function getCursorPosition(event) {
+
+    dot_X = document.getElementById("box-blue").textContent = event.clientX;
+    
+    dot_Y = document.getElementById("box-blue").textContent = event.clientY;
+    
+    }
